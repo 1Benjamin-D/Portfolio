b0095bae4c7b7172bf27ee2c13ac1891bc5e83ff
1Benjamin-D <desmet.benjamin20@orange.fr>
2024-03-19 10:42:46 +0100

    fix .env 3

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -8,14 +8,12 @@ export default async function Skills() {
         <h2 className=' mb-[70px] font-DeadSpaceTitle'>Skills</h2>
         <ul className='grid lg:grid-cols-4 grid-rows-4 gap-6 text-center items-center justify-center  grid-cols-2'>
           {skills.map((skills: any) => (
-            <div className=''>
               <div key={skills.id} className='flex flex-col items-center'>
                 <Image src={skills.image} alt={skills.title} className='' width={1000} height={1000} style={{ width: "80px", height: "auto" }} />
                 <div>
                   <p className=' font-OCRAEXT'>{skills.title}</p>
                 </div>
               </div>
-            </div>
           ))}
         </ul>
       </div>
