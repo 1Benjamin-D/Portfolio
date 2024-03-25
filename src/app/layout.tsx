0e347a7d3d39db0b1639eb20488500b7ef3e14a7
1Benjamin-D <desmet.benjamin20@orange.fr>
2024-03-25 14:15:11 +0100

    FiX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const falconpunchstraight = localFont({
 }); 
 
 const OCRAEXT = localFont({
-  src: '../../public/fonts/ocraext.ttf',
+  src: '../../public/fonts/RNSSanz-Normal.otf',
   display: 'swap',
   variable: '--font-OCRAEXT',
 }); 
